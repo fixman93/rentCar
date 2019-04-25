fecd1d66b5c3c8b4ef3073fc28d89bdb17d2ccc7
Boris Civcic <civcicboris93@gmail.com>
2019-04-25 10:05:47 +0200

    add: Home page --> last added cars section

---
+++ src/components/home/lastAddedItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class LastAddedItem extends Component {
+
+  render() {
+
+    return (
+      <section className='last-added-item'>
+        car
+      </section>
+    )
+  }
+}
+
+
+export default LastAddedItem
