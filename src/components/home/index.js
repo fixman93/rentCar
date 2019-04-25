9bc03c6f3d9330a5161001978ed948331b5fe7e2
Boris Civcic <civcicboris93@gmail.com>
2019-04-25 10:05:18 +0200

    add: Home page --> last added cars section

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import NavBar from '../navbar/index'
+import LastAdded from './lastAdded'
+import hero_bg from '../../assets/home-hero.jpg'
 
 
 import './index.scss'
 class Home extends Component {
 
   render() {
-
+    const herobg = {
+      backgroundImage: `url(${hero_bg})`
+    }
     return (
       <div className='home-page'>
         <NavBar />
+        <section className='home-hero' style={herobg}>
+          <div className='content'>
+            <h1>Find the Best Car</h1>
+            <p>From as low as $10 per day with limited time offer discounts</p>
+          </div>
+        </section>
+        <LastAdded />
       </div>
     )
   }
