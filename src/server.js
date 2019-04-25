d6589686b4cab83e10f579975a0d36433c11898a
Boris Civcic <civcicboris93@gmail.com>
2019-04-25 14:58:52 +0200

    package.json updated

---
+++ src/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express()
+
+app.use(express.static('public'));
+
+app.listen(3000, function () {
+  console.log('Express server is up on port 3000');
+});
